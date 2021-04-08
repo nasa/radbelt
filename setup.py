7941d2a3bdc0024ca4f4ef802074b6d7da325bb7
Leo Singer <leo.singer@ligo.org>
2021-04-08 14:26:20 -0400

    Update required Numpy version to match pyproject.toml

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             ]
         )
     ],
-    install_requires=['astropy', 'numpy'],
+    install_requires=['astropy', 'numpy>=1.20.0'],
     packages=find_packages(),
     package_data={
         'radbelt.extern.igrf': ['*.dat'],
