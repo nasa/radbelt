e9b9e714aa5e5f3d0547d3b173620178206ecff6
Leo Singer <leo.singer@ligo.org>
2022-09-08 12:29:31 +0200

    Convert to pyproject.toml project metadata

--- setup.py
+++ setup.py
@@ -1,25 +1,6 @@
-from setuptools import find_packages
 from numpy.distutils.core import Extension, setup
 
 setup(
-    name='radbelt',
-    description='Astropy-friendly wrapper for the AE-8/AP-8 Van Allen belt model',
-    author='Leo Singer',
-    author_email='leo.p.singer@nasa.gov',
-    version='0.1.0',
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Scientific/Engineering :: Astronomy"
-    ],
-    license='NOSA',
-    python_requires='>=3.7',
     ext_modules=[
         Extension('radbelt.core',
             [
@@ -29,12 +10,5 @@ setup(
                 'radbelt/extern/aep8/trmfun.f'
             ]
         )
-    ],
-    install_requires=['astropy', 'numpy'],
-    packages=find_packages(),
-    package_data={
-        'radbelt.extern.igrf': ['*.dat'],
-        'radbelt.extern.aep8': ['*.asc']
-    },
-    zip_safe=False
+    ]
 )
