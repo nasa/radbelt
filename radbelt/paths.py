cd5b3a20082c65118c67a3d4eeca10f44d21f318
Leo Singer <leo.singer@ligo.org>
2023-11-12 11:43:36 -0500

    Replace deprecated importlib.resources calls

--- radbelt/paths.py
+++ radbelt/paths.py
@@ -6,14 +6,8 @@
 # SPDX-License-Identifier: NASA-1.3
 #
 
-from importlib import resources
+from importlib.resources import files, as_file
 
-from .extern import igrf as igrf_data
-from .extern import aep8 as aep8_data
-
-with resources.path(igrf_data, 'dgrf1945.dat') as p:
-    IGRF_DATA_PATH = str(p.parent.resolve())
-with resources.path(aep8_data, 'ae8min.asc') as p:
-    AEP8_DATA_PATH = str(p.parent.resolve())
-
-del igrf_data, aep8_data
+with as_file(files(__package__)) as path:
+    IGRF_DATA_PATH = str(path / 'extern' / 'igrf')
+    AEP8_DATA_PATH = str(path / 'extern' / 'aep8')
